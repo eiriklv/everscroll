8769b2524149a53518d755f841ac9af8506ce4c5
Eirik Vullum <evullum@gmail.com>
2015-12-13 03:05:15 +0100

    cleanup

--- src/infinite-scroll.js
+++ src/infinite-scroll.js
@@ -3,27 +3,23 @@ module.exports = function({
   disableCallback = false
 }) {
   function getScrollPos(){
-    let yScroll = 0;
-
     if (self.pageYOffset) {
-        yScroll = self.pageYOffset;
+      return self.pageYOffset;
     } else if ((document.documentElement || {}).scrollTop) {
-        yScroll = document.documentElement.scrollTop;
+      return document.documentElement.scrollTop;
     } else if (document.body) {
-        yScroll = document.body.scrollTop;
+      return document.body.scrollTop;
+    } else {
+      return 0;
     }
-
-    return yScroll;
   }
 
   function handleScroll(scroller, event) {
-    // find some relevant values
     const scrollPos = getScrollPos();
     const pageHeight = document.documentElement.scrollHeight;
     const clientHeight = document.documentElement.clientHeight;
     const distanceToBottom = pageHeight - (scrollPos + clientHeight);
 
-    // check if we are within the max distance to the bottom
     if (distanceToBottom < scroller.options.distance) {
       if (!disableCallback && scroller.isUpdating) {
         return;
